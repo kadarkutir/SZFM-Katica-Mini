86cd723954bd9c3af48b5c1bf904dbc0ca0c821e
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-09-24 09:34:16 +0200

    Calculator/clear functions created deleteChar function

--- Project/src/main/java/modell/Calculator.java
+++ Project/src/main/java/modell/Calculator.java
@@ -147,4 +147,20 @@ public class Calculator {
             result += "/";
         }
     }
+
+    /**
+     * Calculating and Clearing functions
+     */
+    public void deleteChar(){
+        if(!(result.equals(" "))){
+            result = removeLastCharOptional(result);
+        }
+    }
+
+    private static String removeLastCharOptional(String s) {
+        return Optional.ofNullable(s)
+                .filter(str -> str.length() != 0)
+                .map(str -> str.substring(0, str.length() - 1))
+                .orElse(s);
+    }
 }
