ca03207d246a55904fc0608f700e2f5a8a62c8bf
AkeSolveig <demeterbeni45@gmail.com>
2022-09-25 20:58:50 +0200

    Created Controller base

---
+++ Project/src/main/java/contoller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import model.*;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class Controller {
+
+    private Calculator calc = new Calculator();
+
+    @FXML
+    private Label result;
+
+    
+}
