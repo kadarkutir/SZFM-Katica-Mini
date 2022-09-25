82c87a99b5da51f9f55daf11cce57b4419a4a2a2
AkeSolveig <demeterbeni45@gmail.com>
2022-09-25 21:04:20 +0200

    Update controller numbers added

--- Project/src/main/java/contoller/Controller.java
+++ Project/src/main/java/contoller/Controller.java
@@ -15,5 +15,52 @@ public class Controller {
     @FXML
     private Label result;
 
-    
+    public void zeroAction(ActionEvent actionEvent){
+        calc.setResultZero();
+        result.setText(calc.getResult());
+    }
+    public void oneAction(ActionEvent actionEvent){
+        calc.setResultOne();
+        result.setText(calc.getResult());
+    }
+
+    public void twoAction(ActionEvent actionEvent){
+        calc.setResultTwo();
+        result.setText(calc.getResult());
+    }
+
+    public void threeAction(ActionEvent actionEvent){
+        calc.setResultThree();
+        result.setText(calc.getResult());
+    }
+
+    public void fourAction(ActionEvent actionEvent){
+        calc.setResultFour();
+        result.setText(calc.getResult());
+    }
+
+    public void fiveAction(ActionEvent actionEvent){
+        calc.setResultFive();
+        result.setText(calc.getResult());
+    }
+
+    public void sixAction(ActionEvent actionEvent){
+        calc.setResultSix();
+        result.setText(calc.getResult());
+    }
+
+    public void sevenAction(ActionEvent actionEvent){
+        calc.setResultSeven();
+        result.setText(calc.getResult());
+    }
+
+    public void eightAction(ActionEvent actionEvent){
+        calc.setResultEight();
+        result.setText(calc.getResult());
+    }
+
+    public void nineAction(ActionEvent actionEvent){
+        calc.setResultNine();
+        result.setText(calc.getResult());
+    }
 }
